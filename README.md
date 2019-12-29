db58c90b939677c885cc7d865c8a7e01c329c65f
Aung Htet <59339257+rat-ah@users.noreply.github.com>
2019-12-29 14:41:23 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,104 @@
+# [ METASPLOIT INSTALLATION TRICKS 1 BY RAT HAT -( Aung Htet ) ]
+
+
+
+requirements in your android device to install metasploit are :~
+
+
+1. 1GB free storage or less than few.
+2. internet speed needed 500mbps total.
+4. during installation don't open any thing.
+
+
+
+NOW 
+
+
+copy these all commands in termux and instal it properly.
+
+
+1. $HOME
+
+2. mkdir RAT_HAT
+
+3. https://github.com/rathat/msf.sh
+
+
+4. chmod +x msf.sh
+
+5. bash msf.sh
+
+=========================
+
+
+
+that's it...............
+
+
+  
+
+
+
+# [ METASPLOIT INSTALLATION TRICKS 2 BY RAT HAT -( Aung Htet ) ]
+
+
+
+
+
+Requirements in your android device to install metasploit are :~
+
+
+1. 1GB free storage or less than few.
+2. internet speed needed 500mbps total.
+4. during installation don't open any thing.
+
+
+
+NOW 
+
+
+
+download METASPLOIT 
+
+
+
+LINK FOR DOWNLOAD  👉  https://anonfile.com/H98fVeq2n7/METASPLOIT_sh
+
+
+
+NOW
+
+
+copy these all commands in termux and install it properly.
+
+
+
+
+
+1. termux-setup-storage
+
+
+
+2. mkdir RAT_HAT
+
+3. cd storage/
+
+4. cd downloads/
+
+5. cp msf.sh data/data/com.termux/RAT_HAT
+
+
+4. $HOME
+
+5. cd RAT_HAT
+
+6. bash msf.sh 
+
+7. msfconsole OR ./msfconsole
+=============================
+
+thats it.......
+
+
+
+[ CYBER SECURITY mm33 😉 ] 
---
+++ msf.sh
@@ -0,0 +1,79 @@
+#!/termux/data/data/bin/bash
+# script by ah from [ RAT_HAT ] NGAPALI
+apt-get update -y
+apt-get upgrade -y
+pkg install python2 -y
+pkg install python -y
+pip install lolcat
+clear
+echo $red 
+echo ' 
+ _______  _______  _______  _______  _______  _______  _______ _________ _        _______   
+(  ____ )(  ____ )(  ___  )(  ____ \(  ____ \(  ____ \(  ____ \\__   __/( (    /|(  ____ \  
+| (    )|| (    )|| (   ) || (    \/| (    \/| (    \/| (    \/   ) (   |  \  ( || (    \/  
+| (____)|| (____)|| |   | || |      | (__    | (_____ | (_____    | |   |   \ | || |        
+|  _____)|     __)| |   | || |      |  __)   (_____  )(_____  )   | |   | (\ \) || | ____   
+| (      | (\ (   | |   | || |      | (            ) |      ) |   | |   | | \   || | \_  )  
+| )      | ) \ \__| (___) || (____/\| (____/\/\____) |/\____) |___) (___| )  \  || (___) |  
+|/       |/   \__/(_______)(_______/(_______/\_______)\_______)\_______/|/    )_)(_______)  
+                                                                                            
+
+/....TuRn On yOur IntErneT coNnecTion....\' | lolcat
+echo " "
+echo "### This Script is written by aung_htet ###" 
+echo " "
+echo "----------- Fetching.../// ---------️----"
+echo " "
+pkg install wget -y
+pkg install openssh -y
+pkg install ruby -y
+pkg install unstable-repo -y
+pkg install metasploit -y
+
+ls
+
+cd ..
+
+ls
+
+cd usr
+
+ls
+
+cd opt
+
+ls
+
+mv metasploit-framework $HOME
+
+ls
+
+cd /data/data/com.termux/files/home
+
+ls
+
+cd metasploit-framework
+
+ls
+
+wget https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt
+
+bash fix-ruby-bigdecimal.sh.txt
+
+echo " "
+echo "############# Completed ################"                                                                                                                       
+echo " "
+echo "If You Are in Internet Then 
+you have downloaded metasploit successfully. 
+add me www.facebook.com/fixit008900
+echo " "
+echo " (x ...BE A WHITE HAT HACKER... x) "
+echo " "
+echo '
+echo " "
+echo "Type msfconsole to start ./msfconsole..."
+echo " "
+
+./msfconsole
+
+exit
